75bf3b96743e2b37ad7932c8be4de63da7d60e3f
Hale Xie <hale.xie@helplightning.com>
2025-09-16 00:00:01 +0800

    visionOS - Fix compiling warnings

--- SVProgressHUD/include/SVIndefiniteAnimatedView.h
+++ SVProgressHUD/include/SVIndefiniteAnimatedView.h
@@ -1,17 +1,1 @@
-//
-//  SVIndefiniteAnimatedView.h
-//  SVProgressHUD, https://github.com/SVProgressHUD/SVProgressHUD
-//
-//  Copyright (c) 2014-2018 Guillaume Campagna. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-@interface SVIndefiniteAnimatedView : UIView
-
-@property (nonatomic, assign) CGFloat strokeThickness;
-@property (nonatomic, assign) CGFloat radius;
-@property (nonatomic, strong) UIColor *strokeColor;
-
-@end
-
+../SVIndefiniteAnimatedView.h
